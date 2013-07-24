f7664600cd86c7fa42fc9480c952bb0797b3a4c5
jku <j.kutnar@me.com>
2013-07-24 22:16:19 +0200

    QR reader for iPhone and iPad including settings panel

--- shp-client-ios/SmartHomePoint/SmartHomePoint/Controllers/Settings/settingsViewController.m
+++ shp-client-ios/SmartHomePoint/SmartHomePoint/Controllers/Settings/settingsViewController.m
@@ -7,8 +7,10 @@
 //
 
 #import "settingsViewController.h"
+#import "ZBarSDK.h"
 
-@interface settingsViewController ()
+
+@interface settingsViewController () <ZBarReaderDelegate>
 
 @end
 
@@ -35,10 +37,34 @@
     // Dispose of any resources that can be recreated.
 }
 
-- (IBAction)onButtonTouchUp:(id)sender {
-    UIStoryboard* storyBoard = [UIStoryboard storyboardWithName:@"MainStoryboard_iPhone" bundle:nil];
-    UIViewController* controller = [storyBoard instantiateInitialViewController];    
-    [[[[UIApplication sharedApplication] delegate] window] setRootViewController:controller];
-    [[[[UIApplication sharedApplication] delegate] window] makeKeyAndVisible];    
+- (IBAction)onButtonTouchUp:(id)sender {    
+    ZBarReaderViewController *reader = [ZBarReaderViewController new];
+    reader.readerDelegate = self;
+    [reader.scanner setSymbology:ZBAR_QRCODE config:ZBAR_CFG_ENABLE to:0];
+    reader.readerView.zoom = 1.0;
+    reader.showsZBarControls = NO;
+    [self presentViewController:reader animated:YES completion:nil];
 }
+
+- (void) imagePickerController:(UIImagePickerController*)reader didFinishPickingMediaWithInfo:(NSDictionary*)info {
+    id<NSFastEnumeration> results = [info objectForKey: ZBarReaderControllerResults];
+    //UIImage *image = [info objectForKey: UIImagePickerControllerOriginalImage];
+    [reader dismissViewControllerAnimated:YES completion:NULL];
+
+
+    /*
+     UIStoryboard* storyBoard = [UIStoryboard storyboardWithName:@"MainStoryboard_iPhone" bundle:nil];
+     UIViewController* controller = [storyBoard instantiateInitialViewController];
+     [[[[UIApplication sharedApplication] delegate] window] setRootViewController:controller];
+     [[[[UIApplication sharedApplication] delegate] window] makeKeyAndVisible];
+     */
+}
+
+- (void) readerControllerDidFailToRead: (ZBarReaderController*) reader withRetry: (BOOL) retry
+{
+    if (!retry)
+        [reader dismissViewControllerAnimated:YES completion:NULL];
+}
+
+
 @end
